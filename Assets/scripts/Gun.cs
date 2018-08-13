7b1469749fe588b8a6094e0a7eced609d75375cb
Laura Cohen <lauramimona@gmail.com>
2018-08-13 12:02:56 +0200

    cleaning up folders

--- Assets/scripts/Gun.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-//allows functionality for all guns and inheritance for different kinds
-public class Gun : Shoot {
-
-
-}
