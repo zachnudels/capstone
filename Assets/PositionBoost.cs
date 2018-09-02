b0ab2bbe3e263f7f37cd678fea48d8652ad9aa09
Brandon Addison <addbra001@myuct.ac.za>
2018-09-02 13:06:08 +0200

    added missing assets

---
+++ Assets/PositionBoost.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionBoost : MonoBehaviour {
+
+	public GameObject particleEffect;
+
+	void OnTriggerEnter (Collider col) {
+		if (col.CompareTag ("Player")) {
+			yBoost(col);
+		}
+	}
+
+	IEnumerator ExecuteAfterTime(float time) {
+		yield return new WaitForSeconds (time);
+	}
+
+	void yBoost(Collider player) {
+		Instantiate (particleEffect, transform.position, transform.rotation);
+		Destroy (particleEffect, 2);
+		Destroy (gameObject);
+
+		//Animator anim = player.GetComponent<Animator> ();
+		//if (anim.getAnimat
+		player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 10.0f, player.transform.position.z);
+
+		//ExecuteAfterTime (3);
+
+		//anim.speed = 1;
+
+		//WaitForSeconds(5);
+
+		//anim.speed = 1;
+	}
+}
---
+++ Assets/SpeedBoost.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject particleEffect;
+	public float mytime;
+	Animator anim;
+
+
+	IEnumerator OnTriggerEnter (Collider col) {
+		if (col.CompareTag ("Player")) {
+			//Speed(col);
+			Instantiate (particleEffect, transform.position, transform.rotation);
+
+			MeshRenderer mesh = GetComponent<MeshRenderer> ();
+			mesh.enabled = false;
+
+			// Disable Mesh Collider here
+
+
+			anim = col.GetComponent<Animator> ();
+
+			anim.speed = 2;
+
+		}
+		Destroy (particleEffect, 1.2f);
+
+		yield return new WaitForSeconds (3);
+		anim.speed = 1;
+		Destroy (gameObject);
+
+	}
+
+	/*
+	void Speed(Collider player) {
+		Instantiate (particleEffect, transform.position, transform.rotation);
+//		Destroy (particleEffect, 2);
+		//Destroy (gameObject);
+
+		anim = player.GetComponent<Animator> ();
+
+		//if (anim.getAnimat
+		//anim.speed = 2;
+		//if (anim.GetCurrentAnimatorStateInfo (0).IsName("Walking")) {
+			anim.speed = 2;
+
+
+	}
+*/
+}
