1cad30da83f503a069724617af2ae5fb702da6b4
Laura Cohen <lauramimona@gmail.com>
2018-09-05 12:20:11 +0200

    Fixed respawn powerup

---
+++ Assets/scripts/CheckPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+	public GameObject particleEffect;
+
+	IEnumerator OnTriggerEnter (Collider col) {
+
+		if (col.CompareTag ("Player")) {
+			//Speed(col);
+			Instantiate (particleEffect, transform.position, transform.rotation);
+
+			MeshRenderer mesh = GetComponent<MeshRenderer> ();
+			mesh.enabled = false;
+
+			// Disable Mesh Collider here
+			MCSAnimationController mcs = col.GetComponent<MCSAnimationController>();
+			mcs.spawner = true;
+
+
+
+		}
+		Destroy (particleEffect, 1.2f);
+
+		yield return new WaitForSeconds (3);
+		//anim.speed = 1;
+		Destroy (gameObject);
+
+	}
+}
