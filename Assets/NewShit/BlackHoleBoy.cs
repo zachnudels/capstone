d3ca76a6062611dc66bad704b71407740825f0e2
Zach Nudelman <zachnudels@gmail.com>
2018-09-06 13:56:08 +0200

    Added SALT_package1

---
+++ Assets/NewShit/BlackHoleBoy.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleBoy : MonoBehaviour {
+
+	private float delta;  // Amount to move left and right from the start point
+	private float speed = 0.5f; 
+	private Vector3 startPos;
+	private bool start;
+	private bool playerIn;
+	private float pullRadius = 20;
+	private float timePassed;
+	private float forceStrength = 1;
+	private bool still = false;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		delta = -2.0f;
+		playerIn = false;
+		start = true;
+		timePassed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shimmy ();
+		pullPlayer();
+		Debug.Log (timePassed);
+		if (timePassed > 20) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (start && delta < -1.0f && other.gameObject.tag == "platform") {
+			Bounds bounds = other.GetComponent<MeshRenderer> ().bounds;
+			delta = bounds.max.x - bounds.min.x;
+			delta /= 3;
+			speed = delta / 12;
+			start = false;
+		} 
+		if (other.gameObject.tag == "Player" && !still) {
+			playerIn = true;
+			still = true;
+		}
+	}
+		
+	//Move side to side
+	void shimmy(){
+		if (!playerIn) {
+			Vector3 v = startPos;
+			v.x += delta * Mathf.Sin (Time.time * speed);
+			transform.position = v;
+		} else {
+			timePassed += Time.deltaTime;
+		}
+	}
+
+	void pullPlayer(){
+		foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)) {
+
+			// apply force on target towards me
+			if (collider.tag == "Player") {
+				if (Mathf.Abs (collider.gameObject.transform.position.y - transform.position.y) < 1) {
+					Vector3 forceDir = transform.position - collider.transform.position;
+					forceDir = new Vector3 (forceDir.x, 0.0f, forceDir.z);
+					collider.gameObject.GetComponentInParent<Rigidbody>().AddRelativeForce(forceDir*forceStrength,ForceMode.Force);
+
+				}
+			}
+		}
+	}
+		
+}
---
+++ Assets/NewShit/MCSAnimationController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCSAnimationController : MonoBehaviour {
+
+
+	private Animator anim;
+	private float walking;
+	private float turning;
+	private bool jump;
+	public int turnSpeed;
+	public float gravity;
+	public float ground = 0.0f;
+
+	public GameObject tp;
+	private Rigidbody RB;
+	//	public AudioSource footsteps;
+
+	public GameObject TPSloc;
+	private int time;
+	private bool inJump;
+	private float jumpTime;
+	private RaycastHit hit;
+	//	public AudioClip footstepClip;
+
+	//	private Vector3 idleGunPos;
+	//	private Quaternion idleGunRot;
+	//	private Vector3 movingGunPos;
+	//	private Quaternion movingGunRot;
+	private Vector3 velocity;
+	//	private Transform idleTrans;
+	// Use this for initialization
+
+	public bool spawner = false;
+	public GameObject SpawnObject;
+	private float spawnx, spawny, spawnz;
+	private bool isWalking;
+
+	void Start () {
+		//		currentCam = tp;
+		anim = GetComponent<Animator> ();
+		walking = 0.0f; 
+		time = 0;
+		turning = 0.0f;
+		inJump = false;
+		jump = false;
+		isWalking = false;
+		Physics.gravity = new Vector3 (0, gravity, 0);
+		RB = GetComponentInParent<Rigidbody> ();
+		//		Debug.Log (footsteps == null);
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		jumpTime += Time.unscaledDeltaTime;
+		//		footsteps.Play ();
+
+		walking = Input.GetAxis ("Vertical");
+		anim.SetFloat ("walking", walking);
+		if (walking > 0.1 && !isWalking) {
+			transform.Find("Footsteps").GetComponent<AudioSource>().Play();
+			transform.Find ("Footsteps").GetComponent<AudioSource> ().volume = 0.3f;
+			isWalking = true;
+		} else if (walking < 0.1 && isWalking || inJump) {
+			transform.Find("Footsteps").GetComponent<AudioSource>().Stop();
+			isWalking = false;
+		}
+
+		turning = Input.GetAxis("Horizontal");
+		transform.Rotate (new Vector3 (0.0f, turnSpeed*turning*Time.deltaTime));
+
+		bool jumpHit = Input.GetKeyDown (KeyCode.Space);
+		bool onGround = Physics.Raycast (transform.position + (new Vector3 (-0.1f, 0.2f, -0.1f)), (Vector3.down), out hit, 1);
+	
+		Debug.DrawRay (transform.position +new Vector3 (-0.1f, 0.2f, -0.1f), (Vector3.down), Color.green);
+		anim.SetBool ("onGround", onGround);
+
+		jump = Input.GetKey (KeyCode.Space);
+		anim.SetBool ("jump", jumpHit);
+		if(jumpHit)
+			Debug.Log (jumpTime);
+
+		if (jumpHit  && jumpTime >1.2f) {
+
+			jumpTime = 0.0f;
+			RB.mass = 0.1f;
+			inJump = true;
+		}
+		if (inJump) {
+			//			Debug.Log (time);
+			//			transform.Translate (Vector3.forward*Time.deltaTime*2);
+			if (time < 7) {
+				RB.AddRelativeForce (new Vector3 (0.0f, 21.0f, 0.0f));
+				//				RB.AddRelativeForce (transform.Find("M3DFemale").forward*6);
+
+
+
+				//				transform.Translate (Vector3.up*Time.deltaTime*3);
+				//				RB.mass = 0.05f;
+			} else if(time > 7 || !jump){
+				//				RB.AddRelativeForce (new Vector3 (0.0f, -21.0f, 0.0f));
+				//				transform.Translate (-Vector3.up*Time.deltaTime*3);
+				//				RB.mass = 70.0f;
+			}
+
+			time++;
+			if (time == 15) {
+				//				transform.Translate (Vector3.forward*Time.deltaTime);
+				time = 0;
+				inJump = false;
+			}
+		}
+		//		Debug.DrawRay (transform.position+(new Vector3(0.0f, 0.1f, 0.0f)), Vector3.down);
+		//		Debug.Log (transform.position);
+		if (!inJump) {
+			bool fall = !Physics.Raycast (transform.position+(new Vector3(0.0f, 0.1f, 0.0f)),(Vector3.down), out hit, 4.5f);
+			anim.SetBool ("fall", fall);
+		}
+
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Roll")) {
+			Debug.Log ("Roll");
+			RB.AddRelativeForce (new Vector3 (0.0f, -5.0f, 0.0f));
+		}
+
+		// Add Checkpoint to Game World with Key Press 'R'
+		if (Input.GetKeyDown (KeyCode.R) && spawner == true) {
+			Instantiate (SpawnObject, transform.position, transform.rotation);
+			spawnx = transform.position.x;
+			spawny = transform.position.y;
+			spawnz = transform.position.z;
+
+			//Debug.Log (SpawnObject.transform.position);
+		}
+
+		//Debug.Log (RB.velocity.y);
+
+		// If falling, move back to Checkpoint
+		if (RB.velocity.y < -55 && spawnx !=0 && spawny != 0 && spawnz != 0) {
+			transform.position = new Vector3 (spawnx, spawny, spawnz);
+		}
+
+
+
+		//		Debug.Log (fall);
+
+
+
+	}
+
+}
