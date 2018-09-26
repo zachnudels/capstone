1d08b2b18070f3c7d7fc77fc3c6818517ad2dba3
Zach Nudelman <zachnudels@gmail.com>
2018-09-26 15:43:12 +0200

    Agent path finding working

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson; 
+
+public class MoveTo : MonoBehaviour {
+	
+	public Transform goal;
+	public ThirdPersonCharacter character;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position; 
+		agent.updateRotation = false;
+	}
+
+	void Update(){
+//		Debug.Log (agent.velocity.y);
+		if (agent.remainingDistance > agent.stoppingDistance) {
+			character.Move (agent.desiredVelocity, false, true);
+		} else {
+			character.Move (Vector3.zero, false, false);
+		}
+
+	}
+
+
+}
---
+++ Assets/PlatformAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAI : MonoBehaviour {
+
+
+	public GameObject[] platforms;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
