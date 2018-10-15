f11d1464c2520c310d0f08dca50cf4fb7a2b3dda
Laura Cohen <lauramimona@gmail.com>
2018-10-16 00:31:35 +0200

    more menu bois

---
+++ Assets/scripts/SettingsMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour {
+
+	public AudioMixer audioMixer;
+
+	public void setVolume(float vol){
+
+		audioMixer.SetFloat ("Volume", vol);
+
+	}
+
+	public void SetFull(bool isFullScreen){
+		Screen.fullScreen = isFullScreen;
+	}
+
+	public void quitToMenu(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
+	}
+
+}
